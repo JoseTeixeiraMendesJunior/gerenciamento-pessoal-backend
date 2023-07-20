43bdac7b8e022a9ad7014243ef681f05f6d19ad5
Jose Teixeira <josejonicaro@gmail.com>
2023-07-20 18:09:51 -0300

    auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['required', 'email', 'exists:users,email'],
+                'password' => ['required'],
+            ], [
+                'email.exists' => 'E-mail ou senha incorreto.',
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'email' => ['E-mail ou senha incorreto.'],
+                ]);
+            }
+
+            $token = $user->createToken($request->email)->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 400);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'min:3'],
+                'email' => ['required', 'email', 'unique:users,email'],
+                'password' => ['required', 'min:8'],
+            ]);
+
+            $data = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ];
+
+            $user = User::create($data);
+
+            return UserResource::make($user);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +17,18 @@ use App\Http\Controllers\Api\TaskController;
 |
 */
 
-Route::group(['prefix' => 'task'], function () {
-    Route::get('', [TaskController::class, 'index'])->name('task.index');
-    Route::post('', [TaskController::class, 'store'])->name('task.store');
-    Route::get('{task}', [TaskController::class, 'show'])->name('task.show');
-    Route::put('{task}', [TaskController::class, 'update'])->name('task.update');
-    Route::delete('{task}', [TaskController::class, 'destroy'])->name('task.destroy');
-});
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('register', [AuthController::class, 'register'])->name('register');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::group(['prefix' => 'task'], function () {
+        Route::get('', [TaskController::class, 'index'])->name('task.index');
+        Route::post('', [TaskController::class, 'store'])->name('task.store');
+        Route::get('{task}', [TaskController::class, 'show'])->name('task.show');
+        Route::put('{task}', [TaskController::class, 'update'])->name('task.update');
+        Route::delete('{task}', [TaskController::class, 'destroy'])->name('task.destroy');
+    });
+
 });
